918ec4f27c0cb7719e4a1e48442b6633d1b21626
whatisgonee <83175057+whatisgonee@users.noreply.github.com>
2021-09-21 18:05:37 +0600

    Create main.js

---
+++ public/main.js
@@ -0,0 +1,81 @@
+var previous;
+var next;
+var search;
+var close;
+var mobileNav;
+var messageClose;
+var archive;
+
+document.addEventListener('DOMContentLoaded', () => {
+	
+
+	var images = document.getElementsByClassName("product-image");
+	var current = 0;
+	var searchForm = document.getElementById("search-form");
+	var searchElem = document.getElementById("search");
+	var opened = false;
+	var hidden = false;
+
+	previous = function() {
+		images[current].classList.add("hidden");
+	
+		current--;
+		if(current < 0) current = images.length-1;
+	
+		images[current].classList.remove("hidden");
+	}
+	
+	next = function() {
+		images[current].classList.add("hidden");
+	
+		current++;
+		if(current > images.length-1) current = 0;
+	
+		images[current].classList.remove("hidden");
+	}
+	
+	search = function() {
+		searchElem.classList.add("hidden");
+		searchForm.style.display = "initial";
+		searchForm.style.zIndex = "8";
+		document.getElementById("searchInput").disabled = false;
+	}
+	
+	close = function() {
+		searchElem.classList.remove("hidden");
+		searchForm.style.display = "none";
+		searchForm.style.zIndex = "-4";
+		document.getElementById("searchInput").disabled = true;
+	}
+	
+	mobileNav = function() {
+		if(opened) {
+			document.getElementById("nav-holder").style.display = "none";
+			document.getElementById("nav-holder").style.width = "0vw";
+		} else {
+			document.getElementById("nav-holder").style.display = "flex";
+			document.getElementById("nav-holder").style.width = "70vw";
+		}
+		
+		opened = !opened;
+	}
+	
+	messageClose = function() {
+		document.getElementById("message").style.display = "none";
+	}
+	
+	archive = function() {
+		if(hidden) {
+			document.getElementById("hide").innerHTML = "<i class=\"fas fa-arrow-up\">";
+			document.getElementById("archived").style.display = "inline-block";
+		} else {
+			document.getElementById("hide").innerHTML = "<i class=\"fas fa-arrow-down\">"
+			document.getElementById("archived").style.display = "none";
+		}
+		
+		hidden = !hidden;
+	}
+	
+	document.getElementById("hide").addEventListener('click', archive);
+	document.getElementById("close").addEventListener('click', close);
+});
